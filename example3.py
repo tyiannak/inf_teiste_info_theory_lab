ce819966abb992eab7bb182dae6f2ba008efdd6e
Theodoros Giannakopoulos <tyiannak@gmail.com>
2016-11-04 01:00:15 +0200

    renamed

--- example3.py
+++ example3.py
@@ -1,38 +1,32 @@
 import ITlib
 import numpy
 import os
-import cv2
 import matplotlib.pyplot as plt
 
-def computeImageEntropy(path):
-    RGB = cv2.imread(path, cv2.CV_LOAD_IMAGE_COLOR)                 # read an image from a jpg file
-    Grayscale = cv2.cvtColor(RGB, cv2.cv.CV_RGB2GRAY)               # convert to grayscale (2D)
-    countValues = numpy.zeros((256,))                               # inialize histogram (counter for each 0..255 pixel value)
-    for i in range(Grayscale.shape[0]):                             # for each row i
-        for j in range(Grayscale.shape[1]):                         # for each collumn j
-            countValues[Grayscale[i,j]] += 1                        # increase the respective grayscale value
-    countValues /= countValues.sum()                                # normalize by sum()
-    H = ITlib.computeEntropy(countValues)                           # compute entropy 
-    return H, Grayscale                                             # return entropy and grayscale
+def generateSymbols(nSymbols):
+    return [numpy.random.choice(["a","b","c"], p=[0.1,0.2,0.7]) for i in range(nSymbols)]
 
-print "\nExample 3"
-print "Compute the entropy of three gray-scale images\n"
-H1, I1 = computeImageEntropy("data" + os.sep + "dot.jpg")
-H2, I2 = computeImageEntropy("data" + os.sep + "seaGray.jpg")
-H3, I3 = computeImageEntropy("data" + os.sep + "cityGray.jpg")
-H4, I4 = computeImageEntropy("data" + os.sep + "noiseGray.jpg")
-Hmax = ITlib.computeMaxEntropy(256)
+def computeProbsFromSymbols(symbols):
+    Pa = float(symbols.count("a")) / len(symbols)
+    Pb = float(symbols.count("b")) / len(symbols)
+    Pc = float(symbols.count("c")) / len(symbols)
+    return (Pa, Pb, Pc)
 
-plt.subplot(2,2,1);
-plt.imshow(I1, cmap='Greys_r')
-plt.title('H = %.2f, P = %.1f%%' % (H1, 100*(1.0-H1/Hmax)))
-plt.subplot(2,2,2);
-plt.imshow(I2, cmap='Greys_r')
-plt.title('H = %.2f, P = %.1f%%' % (H2, 100*(1.0-H2/Hmax)))
-plt.subplot(2,2,3);
-plt.imshow(I3, cmap='Greys_r')
-plt.title('H = %.2f, P = %.1f%%' % (H3, 100*(1.0-H3/Hmax)))
-plt.subplot(2,2,4);
-plt.imshow(I4, cmap='Greys_r')
-plt.title('H = %.2f, P = %.1f%%' % (H4, 100*(1.0-H4/Hmax)))
-plt.show()
+print "\nExample 2"
+print "Simulation of estimating entropy of messages of different length\n"
+
+nSymbols = range(10,20,2)+range(25,105,5)+range(200,2000,100)+range(5000,100000,5000)
+
+Hs = []
+realEntropy = ITlib.computeEntropy(numpy.array([0.1,0.2,0.7]))
+for nS in nSymbols:
+    print "Estimating entropy for %d symbols per message " % nS, 
+    symbols = generateSymbols(nS)
+    (Pa, Pb, Pc) = computeProbsFromSymbols(symbols)
+    Hs.append(ITlib.computeEntropy(numpy.array([Pa, Pb, Pc])))
+    print "P(a)=%.2f P(b)=%.2f P(c)=%.2f --> H=%.3f" % (Pa, Pb, Pc, Hs[-1])
+
+plt.plot(numpy.log10(nSymbols), Hs); 
+plt.plot(numpy.log10(numpy.array([nSymbols[0],nSymbols[-1]])), [realEntropy, realEntropy],"--")
+plt.xlabel("log(nsymbols)"); plt.show()
+
--- example4.py
+++ example4.py
@@ -1,40 +1,38 @@
 import ITlib
-from nltk.corpus import stopwords
 import numpy
-import re
+import os
+import cv2
+import matplotlib.pyplot as plt
 
-stopwords = ["i'm","don't","it's","i'll","you're","the","of","and","to","a","in","for","is","on","that","by","this","with","i","you","it","not","or","be","are","from","at","as","your","all","have","new","more","an","was","we","will","home","can","us","about","if","page","my","has","search","free","but","our","one","other","do","no","information","time","they","site","he","up","may","what","which","their","news","out","use","any","there","see","only","so","his","when","contact","here","business","who","web","also","now","help","get","pm","view","online","c","e","first","am","been","would","how","were","me","s","services","some","these","click","its","like","service","x","than"]
+def computeImageEntropy(path):
+    RGB = cv2.imread(path, cv2.CV_LOAD_IMAGE_COLOR)                 # read an image from a jpg file
+    Grayscale = cv2.cvtColor(RGB, cv2.cv.CV_RGB2GRAY)               # convert to grayscale (2D)
+    countValues = numpy.zeros((256,))                               # inialize histogram (counter for each 0..255 pixel value)
+    for i in range(Grayscale.shape[0]):                             # for each row i
+        for j in range(Grayscale.shape[1]):                         # for each collumn j
+            countValues[Grayscale[i,j]] += 1                        # increase the respective grayscale value
+    countValues /= countValues.sum()                                # normalize by sum()
+    H = ITlib.computeEntropy(countValues)                           # compute entropy 
+    return H, Grayscale                                             # return entropy and grayscale
 
-def readTxtAndComputeEntropy(fileName):
-    print "filename: " + fileName
-    f = open(fileName, "r")
-    text = f.read()
-    f.close()    
-    words = re.findall(r"[\w']+", text)    
-    words2 = [w.lower() for w in words]
-    words3 = [word for word in words2 if word not in stopwords]
-    terms = []
-    weights = []
-    for w in words3:
-        if w.lower() in terms:
-            weights[terms.index(w.lower())] += 1
-        else:
-            terms.append(w.lower())
-            weights.append(1)    
-    terms2 = [t for (w, t) in sorted(zip(weights, terms), reverse = True)]
-    weights2 = numpy.array(sorted(weights, reverse = True)).astype(float)
-    weights  = numpy.array(sorted(weights, reverse = True))
-    weights2 = weights2 / weights2.sum()    
-    H = ITlib.computeEntropy(weights2)
-    Hmax = ITlib.computeMaxEntropy(float(len(terms2)))
-    P = 1 - H/Hmax
-    print "H=%.2f\tHmax=%.2f\tRed.=%.1f%%" % (H, Hmax, 100*P)
+print "\nExample 3"
+print "Compute the entropy of three gray-scale images\n"
+H1, I1 = computeImageEntropy("data" + os.sep + "dot.jpg")
+H2, I2 = computeImageEntropy("data" + os.sep + "seaGray.jpg")
+H3, I3 = computeImageEntropy("data" + os.sep + "cityGray.jpg")
+H4, I4 = computeImageEntropy("data" + os.sep + "noiseGray.jpg")
+Hmax = ITlib.computeMaxEntropy(256)
 
-print "\nExample 4"
-print "Parse the lyrics from 5 songs and compute the entropies (after removing stopwords)\n"
-
-readTxtAndComputeEntropy("data/lyrics/pinkfloyd_time"); print
-readTxtAndComputeEntropy("data/lyrics/eminem_rapgod"); print
-readTxtAndComputeEntropy("data/lyrics/ladygaga_badromance"); print
-readTxtAndComputeEntropy("data/lyrics/beatles_lovemedo"); print
-readTxtAndComputeEntropy("data/lyrics/mosdef_mathematic"); print
+plt.subplot(2,2,1);
+plt.imshow(I1, cmap='Greys_r')
+plt.title('H = %.2f, P = %.1f%%' % (H1, 100*(1.0-H1/Hmax)))
+plt.subplot(2,2,2);
+plt.imshow(I2, cmap='Greys_r')
+plt.title('H = %.2f, P = %.1f%%' % (H2, 100*(1.0-H2/Hmax)))
+plt.subplot(2,2,3);
+plt.imshow(I3, cmap='Greys_r')
+plt.title('H = %.2f, P = %.1f%%' % (H3, 100*(1.0-H3/Hmax)))
+plt.subplot(2,2,4);
+plt.imshow(I4, cmap='Greys_r')
+plt.title('H = %.2f, P = %.1f%%' % (H4, 100*(1.0-H4/Hmax)))
+plt.show()
