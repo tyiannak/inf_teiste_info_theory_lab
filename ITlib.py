1e792a7626be08814b229d873a56f4ea63464fb9
Theodoros Giannakopoulos <tyiannak@gmail.com>
2016-12-06 01:39:30 +0200

    minor change in huffman code generation

--- ITlib.py
+++ ITlib.py
@@ -74,7 +74,7 @@ def generateHuffmanCode(countsChar):
             pair[1] = '0' + pair[1]
         for pair in hi[1:]:
             pair[1] = '1' + pair[1]
-        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+        heappush(heap, [lo[0] + hi[0]+0.0000000001] + lo[1:] + hi[1:])
     huf = sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
     hufDict = {}
     for p in huf:
