c1fc1d3672c1885bd0306af3587b1ab9e127fe65
tyiannak <tyiannak@gmail.com>
2016-12-13 11:37:20 +0200

    minor change in decode

--- ITlib.py
+++ ITlib.py
@@ -153,11 +153,9 @@ def decode(code, text):
     # decodes a coded string using the provided code
     # ARGUMENTS:
     # - code : dictionary of chars->code words
-    # - text : encoded string
+    # - text : encoded string (either list of strings or a concatenated string)
     # RETURNS:
     # - dText: decoded string
-    # NOTE: The input text must be STRING. 
-    # The encode function returns a list of code words. Use "".join() to convert it to string
     dcode = dict((v,k) for k,v in code.iteritems())        
     buff = ""
     dText = ""
